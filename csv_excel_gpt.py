21cfb450174bef0915a5b4fce7701b9b163d9eb1
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2024-10-18 23:38:20 +0530

    Update csv_excel_gpt.py

--- csv_excel_gpt.py
+++ csv_excel_gpt.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from transformers import pipeline
+
 from langchain.prompts import PromptTemplate
 # from langchain.llms import HuggingFacePipeline
 # from langchain import LLMChain
